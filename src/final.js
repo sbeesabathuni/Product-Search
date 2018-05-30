cdd4e21ccfc945c6fced55c521106db0dcc2cc6c
sbeesabathuni <sbeesabathun@cs.stonybrook.edu>
2018-05-30 05:49:21 -0400

    Version 2: Product Search by keyword: 1. Used Promises to read itemIds from csv, make api call for every 20 itemids followed by populating in hashmap. 2. Unit testing using chai and mocha. 3. Documentation

---
+++ src/final.js
@@ -0,0 +1,150 @@
+const hapi = require( 'hapi');
+const server = new hapi.Server();
+//for parsing csv files which contains item ids
+const csv = require("fast-csv");
+const filePath = '/Users/sravya/Desktop/Walmart/public/items.csv';
+const httprequest = require('request');
+
+//For caching item id and description
+const HashMap = require("hashmap");
+server.map = new HashMap();
+
+server.allItemIds = [];
+server.downloaded = false;
+
+//create connection
+server.connection({
+  host:'localhost',
+  port: Number(3000)
+})
+
+//default route
+server.route({
+  method:'GET',
+  path:'/',
+  handler: function (req,res){
+      return res('Walmart Test!! Go to /items/{your keyword} to get the product ids');
+
+    }
+})
+
+//get product info based on ids
+server.downloadBatched = function(ids) {
+    const apiEndpoint = `http://api.walmartlabs.com/v1/items?ids=${ids}&format=json&apiKey=kjybrqfdgp3u4yv2qzcnjndj`;
+    return new Promise(function(done, reject){
+        httprequest.get(apiEndpoint,  (error, response, body) => {
+            if(error) {
+                reject();
+            }
+            done(JSON.parse(body));
+        })
+    });
+}
+
+//read itemIds from csv
+server.readFromCsv = function(filePath) {
+    server.allItemIds = [];
+    return new Promise(function(done, reject){
+        let csvstream = csv.fromPath(filePath)
+            .on("data", function (row) {
+               server.allItemIds.push(row);
+            })
+            .on("end", function () {
+                done("csv is read!!");
+            })
+            .on("error", function (error) {
+                console.log(error);
+                reject();
+            });
+    });
+}
+
+//read itemIds from csv and populate hashmap for every 20 item ids
+server.readCsvAndDownload = function(filePath, callback) {
+    //First readCsv to get item ids
+     server.readFromCsv(filePath).then((res)=> {
+        //For every 20 itemIds make an api call
+        for(var i = 0; i < server.allItemIds.length; i+=20) {
+            // splice i --> i + 20 given max limit is 20
+            var max = 0;;
+            var ids="";
+            if (i+20 < server.allItemIds) {
+                max =  i+20;
+            } else {
+                max =  server.allItemIds.length;
+            }
+            for (var m= i; m<max; m++) {
+                ids += server.allItemIds[m];
+            }
+            var idStr = ids.toString();
+            //api call to get product information
+            return server.downloadBatched(idStr)
+                .then((body) => {
+                    // keep on populating hashmap
+                    //var body =  JSON.parse(body);
+                    var items = [];
+                    if (body != null && body.items != null ) {
+                        items = body.items;
+                    }
+                    for (var i=0; i <items.length; i++) {
+                        server.map.set(items[i].itemId, items[i].longDescription);
+                    }
+                    //end of ids indicated all products have been fetched
+                    if(max >= server.allItemIds.length) {
+                        // set global downloaded = true
+                        server.downloaded = true;
+                        if (callback) {
+                            callback();
+                        }
+                    }
+                });
+        }
+    });
+
+
+};
+
+//check in the hashmap and return the product ids
+function _filterBasedOnKeyword(keyword) {
+  var result = "";
+  server.map.forEach(function(value, key) {
+      if(value.toLowerCase().indexOf(keyword.toLowerCase()) > -1) {
+          if (!result) {
+            result = key;
+          } else {
+            result += "," + key;
+          }
+      }
+  });
+  if (result) {
+    return result;
+  }
+  return "No items matching the given keyword";
+}
+
+//route to get the product ids
+server.route({
+    method: 'GET',
+    path: '/items/{keyword}',
+    config: {
+      handler: function (request, reply) {
+        if (!server.downloaded) {
+            return reply("Please wait for the product information to be downloaded");
+        }
+        var keyword = request.params.keyword;
+        var result = _filterBasedOnKeyword(keyword);
+        return reply(result)
+
+      }
+    }
+  });
+
+server.start((err)=>{
+  if (err) console.log('error while connecting :'+err)
+  else console.log("Server started at localhost:3000");
+})
+
+server.readCsvAndDownload(filePath);
+
+module.exports = server;
+
---
+++ test/products.js
@@ -0,0 +1,113 @@
+const assert = require('chai').assert;
+const server = require('../src/final.js')
+const filePath = '/Users/sravya/Desktop/Walmart/public/items.csv';
+const HashMap = require("hashmap");
+var hmap = new HashMap();
+
+server.register([{
+    register: require('inject-then')
+}])
+
+describe('Testing Product API', function () {
+    it('should validate if server is running', function () {
+        return server.injectThen({
+                method: 'GET',
+                url: '/'
+            })
+            .then(
+                function (response) {
+                    //console.log(response);
+                    assert.deepEqual(response.statusCode, 200);
+                    assert.equal(response.result, 'Walmart Test!! Go to /items/{your keyword} to get the product ids');
+                }
+            )
+    });
+
+    //test read from csv
+    it('should should read ids from csv', function () {
+        return server.readFromCsv(filePath)
+            .then(
+                function (response) {
+                   assert.equal(response, 'csv is read!!');
+                   assert.equal(server.allItemIds.length, 18);
+                }
+            )
+    });
+
+    //test api call
+    it('test product api call', function () {
+        return server.downloadBatched("14225185,14225186")
+            .then(
+                function (response) {
+                   assert.equal(response.items.length, 2);
+                   assert.equal(response.items[0].itemId, 14225185);
+                   assert.equal(response.items[1].itemId, 14225186);
+                }
+            )
+    });
+
+    //check whether hashmap has been populated after retrieving product ids from csv
+    it('validate hashmap', function () {
+        assert.equal(server.downloaded, false);
+        server.readCsvAndDownload(filePath, function(){
+            assert.equal(server.map.size, 18);
+            assert.equal(server.downloaded, true);
+            hmap = server.map;
+        });
+    });
+
+    //final product api with keyword backpack
+    it('check product api when values are not cached in hashmap', function () {
+        server.downloaded = false
+        return server.injectThen({
+                method: 'GET',
+                url: '/items/backpack'
+            })
+            .then(
+                function (response) {
+                    assert.deepEqual(response.statusCode, 200);
+                    assert.equal(response.result, 'Please wait for the product information to be downloaded');
+                }
+            )
+    });
+
+    //final product api with keyword backpack
+    it('check product api when values are cached in hashmap', function () {
+        setTimeout(function(){
+           server.downloaded = true;
+           server.map = hmap;
+           return server.injectThen({
+               method: 'GET',
+               url: '/items/backpack'
+           })
+           .then(
+               function (response) {
+                   console.log(response.result);
+                   assert.deepEqual(response.statusCode, 200);
+                   assert.equal(response.result, '23117408,35613901,35813552');
+               }
+           )
+        }, 5000);
+    });
+
+    //final product api with keyword bag
+        it('check product api when values are cached in hashmap', function () {
+            setTimeout(function(){
+               server.downloaded = true;
+               server.map = hmap;
+               return server.injectThen({
+                   method: 'GET',
+                   url: '/items/bag'
+               })
+               .then(
+                   function (response) {
+                       console.log(response.result);
+                       assert.deepEqual(response.statusCode, 200);
+                       assert.equal(response.result, 'No items matching the given keyword');
+                   }
+               )
+            }, 5000);
+        });
+
+
+});
